3ab2d36970740777f13485d69f3ff131107502e1
David Huang <davidhuang1203@hotmail.com>
2025-01-19 14:41:58 -0600

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,24 +12,8 @@ import {
 } from '@clerk/nextjs'
 
 export const metadata = {
-  title: "AI SDK Python Streaming Preview",
-  description:
-    "Use the Data Stream Protocol to stream chat completions from a Python endpoint (FastAPI) and display them using the useChat hook in your Next.js application.",
-  openGraph: {
-    images: [
-      {
-        url: "/og?title=AI SDK Python Streaming Preview",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    images: [
-      {
-        url: "/og?title=AI SDK Python Streaming Preview",
-      },
-    ],
-  },
+  title: "Nicely",
+  description: "Your therapy companion",
 };
 
 export default function RootLayout({
