32892944cafb3b0bc4d2dd176daf8b2923b4621d
David Huang <davidhuang1203@hotmail.com>
2025-02-07 09:13:56 -0600

    reformat and fix build time error for landing page

--- components/icons.tsx
+++ components/icons.tsx
@@ -1016,3 +1016,24 @@ export const SendIcon = ({
     <polygon points="22 2 15 22 11 13 2 9 22 2" />
   </svg>
 );
+
+export function QuoteIcon(
+  props: React.ComponentProps<"svg"> & { size?: number }
+) {
+  return (
+    <svg
+      width={props.size || 24}
+      height={props.size || 24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M3 21c3 0 7-1 7-8V5c0-1.25-.756-2.017-2-2H4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2 1 0 1 0 1 1v1c0 1-1 2-2 2s-1 .008-1 1.031V20c0 1 0 1 1 1z" />
+      <path d="M15 21c3 0 7-1 7-8V5c0-1.25-.757-2.017-2-2h-4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2h.75c0 2.25.25 4-2.75 4v3c0 1 0 1 1 1z" />
+    </svg>
+  );
+}
